af7a3c308cb35f0ae94b29f479344f3ecf1a2021
AcidicNut <lorinczb96@gmail.com>
2019-05-02 23:49:12 +0200

    MVVM done?

---
+++ FaceAPIHF/FaceAPIHF/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,186 @@
+﻿using FaceAPIHF.Models.Face;
+using Plugin.Media;
+using Plugin.Media.Abstractions;
+using SkiaSharp;
+using SkiaSharp.Views.Forms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace FaceAPIHF.ViewModels
+{
+    class MainPageViewModel : INotifyPropertyChanged
+    {
+        public SKBitmap faceBitmap;
+
+        private StackLayout DataStackLayout;
+
+        public ScrollView DataScrollView;
+
+        private FaceDetection Detector;
+
+        private Grid MainGrid;
+
+        private SKCanvasView MyCanvas;
+
+        public ICommand PhotoCommand { get; private set; }
+        public ICommand FileCommand { get; private set; }
+        public ICommand AnalizeCommand { get; private set; }
+        public ICommand CanvasCommand { get; private set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        public MainPageViewModel(Grid grid, SKCanvasView canvasView)
+        {
+            MainGrid = grid;
+            MyCanvas = canvasView;
+            SetLayout();
+            Detector = new FaceDetection();
+
+            faceBitmap = BitmapExtensions.LoadBitmapResource(GetType(), "FaceAPIHF.kep.png");
+            PhotoCommand = new Command(CameraButton_ClickedAsync);
+            FileCommand = new Command(PickerButton_ClickedAsync);
+            AnalizeCommand = new Command(AnalizeButton_ClickedAsync);
+            MyCanvas.InvalidateSurface();
+        }
+
+        private void SetLayout()
+        {
+            DataStackLayout = new StackLayout();
+            DataScrollView = new ScrollView
+            {
+                Content = DataStackLayout
+            };
+
+            if (Device.Idiom == TargetIdiom.Phone)
+            {
+                // layout views vertically
+                MainGrid.RowDefinitions.Add((new RowDefinition { Height = new GridLength(5, GridUnitType.Star) }));
+                MainGrid.Children.Add(DataScrollView, 0, 2);
+            }
+            else
+            {
+                // layout views horizontally for a larger display (tablet or desktop)
+                MainGrid.Children.Add(DataScrollView, 1, 0);
+                Grid.SetRowSpan(DataScrollView, 3);
+            }
+        }
+
+        // Gomb nyomásra a képen látható arcokat detektálja, kirajzolja és kiírja az adataikat.
+        private async void AnalizeButton_ClickedAsync(object sender)
+        {
+            if (Detector.faceImageByteArray != null)
+            {
+                var detectedFaces = await Detector.DetectAsync();
+                DataStackLayout.Children.Clear();// Create canvas based on bitmap
+                await AnalizeDraw(detectedFaces);
+                MyCanvas.InvalidateSurface();
+                OnPropertyChanged("AnalizeButton_ClickedAsync");
+            }
+        }
+
+        private async Task AnalizeDraw(FaceDetectResponse[] detectedFaces)
+        {
+            using (SKCanvas canvas = new SKCanvas(faceBitmap))
+            {
+                using (SKPaint paint = new SKPaint())
+                {
+                    paint.Style = SKPaintStyle.Stroke;
+                    paint.Color = SKColors.Red;
+                    paint.StrokeWidth = 10;
+                    paint.StrokeCap = SKStrokeCap.Round;
+                    foreach (var face in detectedFaces)
+                    {
+                        canvas.DrawRect(new SKRect(face.FaceRectangle.Left,
+                                                    face.FaceRectangle.Top,
+                           face.FaceRectangle.Left + face.FaceRectangle.Width,
+                           face.FaceRectangle.Top + face.FaceRectangle.Height), paint);
+                        AddDetails(face);
+                    }
+                }
+            }
+        }
+
+        // Egy archoz tartozó adatokat írja ki, ehhez a DataStackLayout-ra Label-öket rak.
+        private void AddDetails(FaceDetectResponse face)
+        {
+            var detailsAdder = new Details();
+            // Code duplication
+            detailsAdder.AddLabelToStackLayout(DataStackLayout, face.FaceAttributes.GetGenericInfo());
+            detailsAdder.AddLabelToStackLayout(DataStackLayout, face.FaceAttributes.FacialHair.ToString);
+            detailsAdder.AddHairDataToStackLayout(DataStackLayout, face.FaceAttributes.Hair);
+            detailsAdder.AddLabelToStackLayout(DataStackLayout, face.FaceRectangle.ToString);
+        }
+
+        // Felhasználónak ad lehetőseget egy kép kiválasztására. Kiválasztás után kirajzolodik.
+        private async void PickerButton_ClickedAsync(object sender)
+        {
+            await CrossMedia.Current.Initialize();
+            try
+            {
+                var file = await CrossMedia.Current.PickPhotoAsync(new PickMediaOptions { });
+
+                if (file == null) return;
+
+                Stream stream = file.GetStream();
+                using (var memoryStream = new MemoryStream())
+                {
+                    stream.CopyTo(memoryStream);
+                    Detector.faceImageByteArray = memoryStream.ToArray();
+                }
+                faceBitmap = SKBitmap.Decode(Detector.faceImageByteArray);
+                MyCanvas.InvalidateSurface();
+                file.Dispose();
+                DataStackLayout.Children.Clear();
+                OnPropertyChanged("PickerButton_ClickedAsync");
+            }
+            catch (Exception ex)
+            {
+                string test = ex.Message;
+            }
+        }
+
+        // Felhasználónak ad lehetőseget egy kép készítésére. Kiválasztás után kirajzolodik.
+        // DefaultCamera = CameraDevice.Front nem működik Androidon.
+        private async void CameraButton_ClickedAsync(object sender)
+        {
+            await CrossMedia.Current.Initialize();
+            try
+            {
+                var photo = await CrossMedia.Current.TakePhotoAsync(new StoreCameraMediaOptions()
+                {
+                    AllowCropping = false,
+                    DefaultCamera = CameraDevice.Front
+                });
+
+                if (photo != null)
+                {
+                    Stream stream = photo.GetStream();
+                    using (var memoryStream = new MemoryStream())
+                    {
+                        stream.CopyTo(memoryStream);
+                        Detector.faceImageByteArray = memoryStream.ToArray();
+                    }
+                    faceBitmap = SKBitmap.Decode(Detector.faceImageByteArray);
+                    MyCanvas.InvalidateSurface();
+                    DataStackLayout.Children.Clear();
+                    OnPropertyChanged("CameraButton_ClickedAsync");
+                }
+            }
+            catch (Exception ex)
+            {
+                string test = ex.Message;
+            }
+        }
+    }
+}
--- FaceAPIHF/FaceAPIHF/Views/MainPage.xaml.cs
+++ FaceAPIHF/FaceAPIHF/Views/MainPage.xaml.cs
@@ -12,69 +12,17 @@ namespace FaceAPIHF
 {
     public partial class MainPage : ContentPage
     {
-        private SKBitmap faceBitmap;
-
-        private StackLayout DataStackLayout;
-
-        private ScrollView DataScrollView;
-
-        private FaceDetection Detector;
+        private MainPageViewModel viewModel;
 
         public MainPage()
         {
             InitializeComponent();
-            SetLayout();
-            Detector = new FaceDetection();
-
-            faceBitmap = BitmapExtensions.LoadBitmapResource(GetType(), "FaceAPIHF.kep.png");
+            //faceBitmap = BitmapExtensions.LoadBitmapResource(GetType(), "FaceAPIHF.kep.png");
+            viewModel = new MainPageViewModel(MainGrid, MyCanvas);
+            BindingContext = viewModel;
 
             SizeChanged += MainPageSizeChanged;
-            MyCanvas.PaintSurface += OnCanvasViewPaintSurface;
-            MyCanvas.InvalidateSurface();
         }
-
-        private void SetLayout()
-        {
-            DataStackLayout = new StackLayout();
-            DataScrollView = new ScrollView
-            {
-                Content = DataStackLayout
-            };
-
-            if (Device.Idiom == TargetIdiom.Phone)
-            {
-                // layout views vertically
-                MainGrid.RowDefinitions.Add((new RowDefinition { Height = new GridLength(5, GridUnitType.Star) }));
-                MainGrid.Children.Add(DataScrollView, 0, 2);
-            }
-            else
-            {
-                // layout views horizontally for a larger display (tablet or desktop)
-                MainGrid.Children.Add(DataScrollView, 1, 0);
-                Grid.SetRowSpan(DataScrollView, 3);
-            }
-        }
-
-        // Tudom hogy ez platform specifikus, de nekem ez így tetszik
-        private void MainPageSizeChanged(object sender, EventArgs e)
-        {
-            if (Device.Idiom == TargetIdiom.Phone)
-            {
-                if (Width > Height)
-                {
-                    MainGrid.Children.Remove(DataScrollView);
-                    MainGrid.Children.Add(DataScrollView, 1, 0);
-                    Grid.SetRowSpan(DataScrollView, 3);
-                }
-                else
-                {
-                    MainGrid.Children.Remove(DataScrollView);
-                    MainGrid.Children.Add(DataScrollView, 0, 2);
-                    Grid.SetRowSpan(DataScrollView, 1);
-                }
-            }
-        }
-
         // SKiaSharp Canvas kirajzolásért felelős event handler-je. 
         // A canvas InvalidateSurface() függvényének hívásakor is lefut.
         private void OnCanvasViewPaintSurface(object sender, SKPaintSurfaceEventArgs args)
@@ -84,105 +32,27 @@ namespace FaceAPIHF
             SKCanvas canvas = surface.Canvas;
 
             canvas.Clear();
-            canvas.DrawBitmap(faceBitmap, info.Rect, BitmapStretch.Uniform);
+            if (viewModel.faceBitmap != null)
+                canvas.DrawBitmap(viewModel.faceBitmap, info.Rect, BitmapStretch.Uniform);
         }
 
-        // Gomb nyomásra a képen látható arcokat detektálja, kirajzolja és kiírja az adataikat.
-        private async void AnalizeButton_ClickedAsync(object sender, EventArgs e)
+        // Tudom hogy ez platform specifikus, de nekem ez így tetszik
+        private void MainPageSizeChanged(object sender, EventArgs e)
         {
-            if (Detector.faceImageByteArray != null)
+            if (Device.Idiom == TargetIdiom.Phone)
             {
-                var detectedFaces = await Detector.DetectAsync();
-                DataStackLayout.Children.Clear();// Create canvas based on bitmap
-                using (SKCanvas canvas = new SKCanvas(faceBitmap))
+                if (Width > Height)
                 {
-                    using (SKPaint paint = new SKPaint())
-                    {
-                        paint.Style = SKPaintStyle.Stroke;
-                        paint.Color = SKColors.Red;
-                        paint.StrokeWidth = 10;
-                        paint.StrokeCap = SKStrokeCap.Round;
-                        foreach (var face in detectedFaces)
-                        {
-                            canvas.DrawRect(new SKRect(face.FaceRectangle.Left,
-                                                        face.FaceRectangle.Top,
-                               face.FaceRectangle.Left + face.FaceRectangle.Width,
-                               face.FaceRectangle.Top + face.FaceRectangle.Height), paint);
-                            AddDetails(face);
-                        }
-                    }
+                    MainGrid.Children.Remove(viewModel.DataScrollView);
+                    MainGrid.Children.Add(viewModel.DataScrollView, 1, 0);
+                    Grid.SetRowSpan(viewModel.DataScrollView, 3);
                 }
-                MyCanvas.InvalidateSurface();
-            }
-        }
-
-        // Egy archoz tartozó adatokat írja ki, ehhez a DataStackLayout-ra Label-öket rak.
-        private void AddDetails(FaceDetectResponse face)
-        {
-            var detailsAdder = new Details();
-            // Code duplication
-            detailsAdder.AddLabelToStackLayout(DataStackLayout, face.FaceAttributes.GetGenericInfo());
-            detailsAdder.AddLabelToStackLayout(DataStackLayout, face.FaceAttributes.FacialHair.ToString);
-            detailsAdder.AddHairDataToStackLayout(DataStackLayout, face.FaceAttributes.Hair);
-            detailsAdder.AddLabelToStackLayout(DataStackLayout, face.FaceRectangle.ToString);
-        }
-
-        // Felhasználónak ad lehetőseget egy kép kiválasztására. Kiválasztás után kirajzolodik.
-        private async void PickerButton_ClickedAsync(object sender, EventArgs e)
-        {
-            await CrossMedia.Current.Initialize();
-            try
-            {
-                var file = await CrossMedia.Current.PickPhotoAsync(new PickMediaOptions { });
-
-                if (file == null) return;
-
-                Stream stream = file.GetStream();
-                using (var memoryStream = new MemoryStream())
+                else
                 {
-                    stream.CopyTo(memoryStream);
-                    Detector.faceImageByteArray = memoryStream.ToArray();
+                    MainGrid.Children.Remove(viewModel.DataScrollView);
+                    MainGrid.Children.Add(viewModel.DataScrollView, 0, 2);
+                    Grid.SetRowSpan(viewModel.DataScrollView, 1);
                 }
-                faceBitmap = SKBitmap.Decode(Detector.faceImageByteArray);
-                MyCanvas.InvalidateSurface();
-                file.Dispose();
-                DataStackLayout.Children.Clear();
-            }
-            catch (Exception ex)
-            {
-                string test = ex.Message;
-            }
-        }
-
-        // Felhasználónak ad lehetőseget egy kép készítésére. Kiválasztás után kirajzolodik.
-        // DefaultCamera = CameraDevice.Front nem működik Androidon.
-        private async void CameraButton_ClickedAsync(object sender, EventArgs e)
-        {
-            await CrossMedia.Current.Initialize();
-            try
-            {
-                var photo = await CrossMedia.Current.TakePhotoAsync(new StoreCameraMediaOptions()
-                {
-                    AllowCropping = false,
-                    DefaultCamera = CameraDevice.Front
-                });
-
-                if (photo != null)
-                {
-                    Stream stream = photo.GetStream();
-                    using (var memoryStream = new MemoryStream())
-                    {
-                        stream.CopyTo(memoryStream);
-                        Detector.faceImageByteArray = memoryStream.ToArray();
-                    }
-                    faceBitmap = SKBitmap.Decode(Detector.faceImageByteArray);
-                    MyCanvas.InvalidateSurface();
-                    DataStackLayout.Children.Clear();
-                }
-            }
-            catch (Exception ex)
-            {
-                string test = ex.Message;
             }
         }
     }
